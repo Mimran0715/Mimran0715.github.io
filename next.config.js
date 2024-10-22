d6340d410ba39eb93b71d751a3d1906ae4481160
Mimran0715 <maleeha.imranm@gmail.com>
2024-10-21 21:16:33 -0700

    committing

--- next.config.js
+++ next.config.js
@@ -1,27 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    basePath: "/mimran0715/github.io",
+  output: "export",
+  basePath: "/mimran0715/github.io",
   
-    images: {
-      unoptimized: true,
-    },
-    webpack: (config) => {
-      config.module.rules.push({
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1,
-            },
-          },
-          'postcss-loader',
-        ],
-      });
-      return config;
-    },
-  };
-  
-  export default nextConfig;
+  images: {
+    unoptimized: true,
+  },
+
+  // No need for custom Webpack configuration for CSS
+};
+
+export default nextConfig;
