68d4a3e502d1b70d6575dbcd25a77f68142f11e9
Mimran0715 <maleeha.imranm@gmail.com>
2026-04-15 14:14:54 -0700

    fixed issue

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,9 @@ import Header from "./components/Header"
 import Projects from "./components/Projects"
 import Footer from "./components/Footer"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
-
 export default function App() {
   return (
-    <div style={{
-      maxWidth: 1000,
-      margin: "0 auto",
-      padding: "60px 24px"
-    }}>
+    <div>
       <Header />
       <Projects />
       <Footer />
